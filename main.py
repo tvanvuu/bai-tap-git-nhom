1e6e261c7e1ee4b6ff298c717c39a30cf6db476a
ntdien31@gmail.com <ntdien31@gmail.com>
2026-06-09 13:53:46 +0700

    feat:dev A tinh thue co giam tru gia canh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def tinh_thue_thu_nhap(thu_nhap):
   # Cập nhật công thức Thuế tại đây
-  return 0
+  thu_nhap_tinh_thue = thu_nhap - 4000000
+  return max (0, thu_nhap_tinh_thue * 0.05)
 
 print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
